e1c8193687f826c04edab136301a6560f002cbe2
Karyn Herrera <karynherrera@gmail.com>
2018-10-08 12:27:27 -0300

    se añaden componentes y vista principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainPinsContainerComponent } from './main-pins-container/main-pins-container.component';
+import { PublicacionesComponent } from './publicaciones/publicaciones.component';
+import { TagsComponent } from './tags/tags.component';
 
 //Importaciones Material Angular
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPinsContainerComponent
+    MainPinsContainerComponent,
+    PublicacionesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
